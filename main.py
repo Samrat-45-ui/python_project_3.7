72afb07a4a45354d372735a1650f0ab6fc8d353a
Samrat-45-ui <22sshrestha@wakatipu.school.nz>
2026-04-23 13:47:17 +1200

    I created intial files, made the blueprint kinda of a support class and the GUI class and also initialised the github repository.

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import messagebox
+
+class Person:
+    def __init__(self, name, age, book, isbn):
+        self.name = name
+        self.age = age
+        self.book = book
+        self.isbn = isbn
+
+class LibraryInv:
+    def __init__(self):
+        pass
