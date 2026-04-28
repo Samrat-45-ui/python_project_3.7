f305f0f10606b6543a497d02408c5cefb3bca063
Samrat-45-ui <22sshrestha@wakatipu.school.nz>
2026-04-28 20:21:47 +1200

    Today, I essentially create a json file to allow my program to have persistant storage feature. Along with that, I create and put in some styling to my GUI.

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 
-class Person:
+class Borrower:
     def __init__(self, name, age, book, isbn):
         self.name = name
         self.age = age
         self.book = book
         self.isbn = isbn
 
+    def todict(self):
+        return{
+            "name": self.name,
+            "age": self.age,
+            "book": self.book,
+            "isbn": self.isbn,
+        }
+
 class LibraryInv:
-    def __init__(self):
-        pass
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Jaydon's Library")
+        self.root.geometry("450x600")
+        self.root.configure(bg="#2c3e50")
+
+        Label(root, text="LIBRARY MANAGEMENT", font=("Arial", 16, "bold")).pack(pady=20)
+
+        self.fields = {}
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    LibraryInv(root)
+    root.mainloop()
+        
+
+        
+
+        
