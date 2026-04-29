6307161b3bd79ebc4554fe94241a7f2efe0e684d
Samrat-45-ui <22sshrestha@wakatipu.school.nz>
2026-04-29 13:47:53 +1200

    Firstly, I added the input boxes for a variety of fields, which are the name of borrower, age of the borrower, the book's name and it's isbn.
    Along with that styled the fields to be anchored to each other along with that I setup a default padding inside the input box; moreover, those input boxes and their titles have been padded so that the GUI looks top-knotch.
    
    Added 3 different buttons with their respective buttons that allows the user to issue book, view all the borrowers and also to return the book or remove the borrower.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from tkinter import *
 from tkinter import messagebox
 
+
 class Borrower:
     def __init__(self, name, age, book, isbn):
         self.name = name
@@ -17,25 +18,50 @@ class Borrower:
             "isbn": self.isbn,
         }
 
+
 class LibraryInv:
     def __init__(self, root):
         self.root = root
-        self.root.title("Jaydon's Library")
+        self.root.title("Newa's Library")
         self.root.geometry("450x600")
         self.root.configure(bg="#2c3e50")
 
         Label(root, text="LIBRARY MANAGEMENT", font=("Arial", 16, "bold")).pack(pady=20)
 
-        self.fields = {}
+        label_style = {"font": ("Arial", 10, "bold")}
+        input_box_style = { "font": ("Arial", 10)}
 
+        self.fields = {}
+        for label_text, var_name in [("Borrower Name", "name"), ("Age", "age"), ("Book Title", "book"), ("ISBN", "isbn")]:
+            Label(root, text=label_text, **label_style).pack(anchor="w", padx=50)
+            input_box = Entry(root, **input_box_style)
+            input_box.pack(fill="x", padx=50, pady=5, ipady=3)
+            self.fields[var_name] = input_box
+
+        issue_book_btn = Button(text="Issue Book", command=self.issue_book)
+        issue_book_btn.pack(padx=20, pady=20)
+
+        view_borrower_btn = Button(text="View all Borrowers", command=self.view_borrower)
+        view_borrower_btn.pack(padx=15, pady=15)
+
+        return_book_btn = Button(text="Return Book", command=self.return_book)
+        return_book_btn.pack(padx=15, pady=15)
+
+    def issue_book(self):
+        try:
+            print("hello")
+
+        except ValueError:
+            print("Please, input valid information.")
+
+    def view_borrower(self):
+        pass
+
+    def return_book(self):
+        pass
 
 
 if __name__ == "__main__":
     root = Tk()
     LibraryInv(root)
     root.mainloop()
-        
-
-        
-
-        
